6b69d5deab26b40c91405d18108324a65bcd3159
Akash Krishna <80200577+Akash-193@users.noreply.github.com>
2025-07-21 00:38:05 +0800

    Update index.js

--- sanity/schemaTypes/index.js
+++ sanity/schemaTypes/index.js
@@ -1,4 +1,4 @@
 import content from './content'
 import publication from './publications' // <-- Import the new schema
 
-export const schemaTypes = [content, publications] // <-- Add it to the array
+export const schemaTypes = [content, publication] // <-- Add it to the array
